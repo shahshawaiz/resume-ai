d3a9f5b9f984e94f70cc04215cd34f3cf6e160ef
Shawaiz <shawaiz.shah@lhg.lovehoneygroup.com>
2024-03-03 21:57:06 +0100

    Add Socials

--- client/app/pages/index.js
+++ client/app/pages/index.js
@@ -20,6 +20,20 @@ export default function Home() {
       </Head>
       <FileUploader onSuccess={onSuccess} />
       <ToastContainer />
+      <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <span>Developed by Shawaiz Shah</span>
+        <a href="https://www.linkedin.com/in/shawaiz-shah-32b40537" target="_blank" rel="noopener noreferrer">
+          <img src="/linkedin.png" alt="LinkedIn" className={styles.icon} /> LinkedIn
+        </a>
+        <a href="https://twitter.com/shasaheb" target="_blank" rel="noopener noreferrer">
+          <img src="/x.png" alt="X" className={styles.icon} /> X
+        </a>        
+        <a href="https://www.buymeacoffee.com/yourusername" target="_blank" rel="noopener noreferrer">
+          <img src="/buy-me-a-coffee.png" alt="Buy Me a Coffee" className={styles.icon} /> Buy Me a Coffee
+        </a>
+      </div>
+    </footer>
     </div>
   );
 }
